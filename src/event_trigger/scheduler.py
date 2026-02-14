ac9ddd2d5b174e6050d5544db9dde2e17cc340da
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-02-14 15:58:05 -0500

    Move Scheduler protocol to typing folder

--- src/event_trigger/scheduler.py
+++ src/event_trigger/scheduler.py
@@ -2,29 +2,12 @@ from __future__ import annotations
 
 import asyncio
 import threading
-from typing import Any, Protocol, TYPE_CHECKING
+from typing import Any, TYPE_CHECKING
+
+from .typing.scheduler import Scheduler
 
 if TYPE_CHECKING:
     from collections.abc import Callable
-
-
-class Scheduler(Protocol):
-    """
-    Object used for following through on callback registered to events.
-    """
-
-    def schedule(self, callback: Callable, *args, **kwds) -> None:
-        """
-        Schedules a callback to be called.
-
-        :param callback: Method or function to be scheduled.
-        :type callback: Callable
-        :param args: Pass-through positional arguments for _callback_.
-        :type args: Any
-        :param kwds: Pass-through keyword arguments for _callback_.
-        :type kwds: Any
-        """
-        ...
 
 
 class SyncScheduler(Scheduler):
---
+++ src/event_trigger/typing/scheduler.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import Protocol, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+
+class Scheduler(Protocol):
+    """
+    Object used for following through on callback registered to events.
+    """
+
+    def schedule(self, callback: Callable, *args, **kwds) -> None:
+        """
+        Schedules a callback to be called.
+
+        :param callback: Method or function to be scheduled.
+        :type callback: Callable
+        :param args: Pass-through positional arguments for _callback_.
+        :type args: Any
+        :param kwds: Pass-through keyword arguments for _callback_.
+        :type kwds: Any
+        """
+        ...

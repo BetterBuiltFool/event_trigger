19c80f0b451014fc674b974d5be5fce18875e812
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-03-06 13:54:08 -0500

    Fix config tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,34 +8,34 @@ class TestConfig(unittest.TestCase):
     def tearDown(self) -> None:
         config(scheduler.DEFAULT(), runner.DEFAULT())
 
-    def test_scheduler_no_threader(self) -> None:
+    def test_scheduler_no_runner(self) -> None:
         """
-        Changing only the scheduler, see if the threader remains default.
+        Changing only the scheduler, see if the runner remains default.
         """
 
         config(scheduler=scheduler.QueueScheduler())
 
         self.assertIsInstance(scheduler._active_scheduler, scheduler.QueueScheduler)
-        self.assertIsInstance(runner._active_threader, runner.DEFAULT)
+        self.assertIsInstance(runner._active_runner, runner.DEFAULT)
 
-    def test_threader_no_scheduler(self) -> None:
+    def test_runner_no_scheduler(self) -> None:
         """
-        Changing only the threader, see if the scheduler remains default.
+        Changing only the runner, see if the scheduler remains default.
         """
 
-        config(threader=runner.AsyncioThreader())
+        config(runner=runner.AsyncioRunner())
 
         self.assertIsInstance(scheduler._active_scheduler, scheduler.DEFAULT)
-        self.assertIsInstance(runner._active_threader, runner.AsyncioThreader)
+        self.assertIsInstance(runner._active_runner, runner.AsyncioRunner)
 
     def test_both(self) -> None:
         """
         Ensure that both change appropriately.
         """
 
-        config(scheduler=scheduler.QueueScheduler(), threader=runner.AsyncioThreader())
+        config(scheduler=scheduler.QueueScheduler(), runner=runner.AsyncioRunner())
 
-        self.assertIsInstance(runner._active_threader, runner.AsyncioThreader)
+        self.assertIsInstance(runner._active_runner, runner.AsyncioRunner)
         self.assertIsInstance(scheduler._active_scheduler, scheduler.QueueScheduler)
 
 

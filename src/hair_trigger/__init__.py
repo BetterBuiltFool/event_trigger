bd10e5301f5573b16b4cdd36ff4c8f220a77dfb5
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-03-16 13:36:47 -0400

    Add re-export in inits

--- src/hair_trigger/__init__.py
+++ src/hair_trigger/__init__.py
@@ -1,3 +1,3 @@
-from .configure import config  # noqa:F401
-from .event import Event  # noqa:F401
-from .scheduler import pump_events  # noqa:F401
+from .configure import config as config  # noqa:F401
+from .event import Event as Event  # noqa:F401
+from .scheduler import pump_events as pump_events  # noqa:F401
--- src/hair_trigger/typing/__init__.py
+++ src/hair_trigger/typing/__init__.py
@@ -1,2 +1,2 @@
-from .scheduler import Scheduler  # noqa: F401
-from .runner import Runner  # noqa: F401
+from .scheduler import Scheduler as Scheduler  # noqa: F401
+from .runner import Runner as Runner  # noqa: F401
